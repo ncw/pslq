ae1005d3ab0740b701235255f46c49ee69786a52
Nick Craig-Wood <nick@craig-wood.com>
2022-10-31 10:13:48 +0000

    pslq binary: add -log-max-coeff to set maximum coefficient size

--- pslq/main.go
+++ pslq/main.go
@@ -17,12 +17,13 @@ import (
 )
 
 var (
-	verbose              = flag.Bool("verbose", false, "Print lots of stuff while running")
-	iterations           = flag.Int("iterations", 1000, "Number of iterations to use max")
-	prec                 = flag.Uint("prec", 64, "Precision to use (bits)")
-	needFirst            = flag.Bool("need-first", false, "Retry if first entry is not used")
-	stdin      io.Reader = os.Stdin
-	stdout     io.Writer = os.Stdout
+	verbose               = flag.Bool("verbose", false, "Print lots of stuff while running")
+	iterations            = flag.Int("iterations", 1000, "Number of iterations to use max")
+	prec                  = flag.Uint("prec", 64, "Precision to use (bits)")
+	logMaxCoeff           = flag.Uint("log-max-coeff", 64, "log2(max coefficient size)")
+	needFirst             = flag.Bool("need-first", false, "Retry if first entry is not used")
+	stdin       io.Reader = os.Stdin
+	stdout      io.Writer = os.Stdout
 )
 
 // syntaxError prints the syntax
@@ -156,7 +157,11 @@ func main() {
 		fmt.Fprintf(stdout, "x[%d] = %*.*f\n", i, digits+6, digits, x)
 	}
 
-	pslq := pslq.New(*prec).SetMaxSteps(*iterations).SetVerbose(*verbose)
+	// max coefficient is 2^logMaxCoeff
+	maxCoeff := big.NewInt(1)
+	maxCoeff.Lsh(maxCoeff, *logMaxCoeff)
+
+	pslq := pslq.New(*prec).SetMaxSteps(*iterations).SetVerbose(*verbose).SetMaxCoeff(maxCoeff)
 	err := run(pslq, digits, xs)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "PSLQ failed: %v\n", err)
